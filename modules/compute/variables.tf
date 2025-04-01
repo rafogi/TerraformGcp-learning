eeacaeddf07db6ed2d2fa7525cb3eef886dc310c
Raphael Cordero <raffycordero2001@yahoo.ca>
2025-03-31 22:57:52 -0700

    modified variables.tf file in compute

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,11 +1,28 @@
-variable "bucket_name" {
-  description = "The name of the storage bucket"
+variable "vpc_network_name" {
+  description = "The name of the VPC network"
   type        = string
-  default     = "terraform-storage-bucket"
 }
 
-variable "bucket_location" {
-  description = "The location of the storage bucket"
+variable "instance_count" {
+  description = "The number of compute instances"
+  type        = number
+  default     = 2
+}
+
+variable "instance_name_prefix" {
+  description = "The prefix for the instance names"
   type        = string
-  default     = "US"
+  default     = "ubuntu-instance"
+}
+
+variable "machine_type" {
+  description = "The machine type for the instances"
+  type        = string
+  default     = "e2-micro"
+}
+
+variable "image" {
+  description = "The image for the instances"
+  type        = string
+  default     = "ubuntu-os-cloud/ubuntu-2004-lts"
 }
