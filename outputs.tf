48a58597689f792124c63459bd92e274ec08845a
Raphael Cordero <raffycordero2001@yahoo.ca>
2025-04-04 21:34:16 -0700

    rename dockerfile

--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_ips" {
-  value = google_compute_instance.ubuntu_instance.*.network_interface[0].access_config[0].nat_ip
-}
-
-output "bucket_name" {
-  value = google_storage_bucket.bucket.name
-}
