dab6abc202aa8f08229f265e6bac025bc90bfa94
Raphael Cordero <raffycordero2001@yahoo.ca>
2025-04-06 23:23:33 -0700

    modified app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello World! i hope this is working.");
+    res.send("Hello World! i hope this is working on gke!.");
 })
 app.listen(80, () => {
     console.log('Server is running on port 80');
