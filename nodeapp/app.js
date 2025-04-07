340a7934d2ebebe927d8896d276124f092bd521a
Raphael Cordero <raffycordero2001@yahoo.ca>
2025-04-06 22:17:00 -0700

    change write

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get("/", (req, res) => {
-    res.send("Hello World! This is a Node.js application running in a Docker container.");
+    res.send("Hello World! i hope this is working.");
 })
 app.listen(80, () => {
     console.log('Server is running on port 80');
